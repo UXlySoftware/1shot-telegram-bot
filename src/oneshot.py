228e3b64a4b2005a0560eccad1fbb7f22eef88fd
TtheBC01 <tac688@alumni.stanford.edu>
2025-05-02 20:13:51 -0700

    hints

--- src/oneshot.py
+++ src/oneshot.py
@@ -7,5 +7,6 @@ API_KEY = os.getenv("ONESHOT_API_KEY")
 API_SECRET = os.getenv("ONESHOT_API_SECRET")
 BUSINESS_ID = os.getenv("ONESHOT_BUSINESS_ID") 
 
-# import the the 1shot client with your API key and secret from your 1Shot Org (https://docs.1shotapi.com/org-creation.html)           
+# import the the 1shot client with your API key and secret from your 1Shot Org (https://docs.1shotapi.com/org-creation.html)  
+# its handy to instantiate it in a single location and import the singlton where you need it         
 oneshot_client = AsyncClient(api_key=API_KEY, api_secret=API_SECRET)
